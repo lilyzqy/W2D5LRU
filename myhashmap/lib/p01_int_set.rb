e7368d49d7a94524ad540bf87448ecafd116f4ab
lily <yang>
2017-10-13 15:14:24 -0700

    Done with parts 1 and 2.

--- myhashmap/lib/p01_int_set.rb
+++ myhashmap/lib/p01_int_set.rb
@@ -1,22 +1,31 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    (0..@store.length).to_a.include?(num)
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
@@ -27,18 +36,24 @@ class IntSet
   end
 
   def insert(num)
+    unless include?(num)
+      bucket(num) << num
+    end
   end
 
   def remove(num)
+    bucket(num).delete(num)
   end
 
   def include?(num)
+    bucket(num).include?(num)
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def bucket(num)
+    bucket_number = num % num_buckets
+    @store[bucket_number]
   end
 
   def num_buckets
@@ -47,26 +62,36 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @numbers = []
   end
 
   def insert(num)
+    unless include?(num)
+      bucket(num) << num
+      @count += 1
+      @numbers << num
+    end
+    resize! if @count > num_buckets
   end
 
   def remove(num)
+    bucket(num).delete(num)
   end
 
   def include?(num)
+    bucket(num).include?(num)
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def bucket(num)
+    bucket_number = num % num_buckets
+    @store[bucket_number]
   end
 
   def num_buckets
@@ -74,5 +99,12 @@ class ResizingIntSet
   end
 
   def resize!
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    numbers = @numbers.dup
+    @numbers = []
+    numbers.each do |num|
+      insert(num)
+    end
   end
 end
--- myhashmap/lib/p02_hashing.rb
+++ myhashmap/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    hold = 0
+    self.each_with_index do |e, idx|
+      hold += (e.hash * idx.hash)
+    end
+    hold
   end
 end
 
 class String
   def hash
+    arr = self.chars
+    hold = 1
+    arr.each_with_index do |e, idx|
+      hold += e.ord.hash / idx.hash
+    end
+    hold
   end
 end
 
@@ -16,6 +27,14 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hold = 2
+    self.each do |key, value|
+      if key.is_a?(Symbol)
+      hold += key.object_id.hash * value.hash
+      else
+      hold += key.hash / value.hash
+      end
+    end
+    hold
   end
 end
